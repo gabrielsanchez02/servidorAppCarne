24494ca46c0dbcff7f567ce8c03d115d721985eb
gabrielsanchez02 <32426227+gabrielsanchez02@users.noreply.github.com>
2020-11-02 10:54:47 -0300

    new

--- build/controllers/indexcontroller.js
+++ build/controllers/indexcontroller.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexcontroller = void 0;
 class Indexcontroller {
     index(req, res) {
         res.send('hello');
